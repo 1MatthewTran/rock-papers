1c200b94b70f25edd097bb27d2eec7998169ba28
1MatthewTran <1matthewtran@gmail.com>
2023-10-01 19:41:14 -0400

    add ui

--- rockPaperScissor.js
+++ rockPaperScissor.js
@@ -1,3 +1,11 @@
+const rockButton = document.querySelector('.rock');
+const paperButton = document.querySelector('.paper');
+const scissorsButton = document.querySelector('.scissors');
+const gameResult = document.querySelector('.gameResult');
+const record = document.querySelector('.record');
+const series = document.querySelector('.seriesResult');
+let wins=0, losses=0, ties=0;
+
 function getComputerChoice(){
     const randInt = Math.floor(Math.random()* 3)
     if(randInt === 0){
@@ -8,63 +16,63 @@ function getComputerChoice(){
     }
     return 'scissors';
 }
-function playRound(playerChoice, computerChoice){
-    const choice = playerChoice.toLowerCase();
-    if (playerChoice === computerChoice){
-        return "Tie! You both chose the same thing.";
+
+function displayScore(){
+    series.textContent = '';
+    record.textContent= `Wins: ${wins}, Losses: ${losses}, Ties: ${ties}`;
+    if(wins===3){
+        series.textContent= 'Congradumalations! You won the series!';
+        wins=0;
+        losses =0;
+        ties= 0;
     }
-    if(choice==='rock'){
-        if(computerChoice==='paper'){
-            return "Paper beats Rock! You Lose!";
-        }
-        return "Rock beats Scissors! You Win!";
+    if(losses===3){
+        series.textContent= 'You lost! Get better bruh!';
+        wins=0;
+        losses =0;
+        ties= 0;
     }
-    if(choice==='paper'){
-        if(computerChoice==='scissors'){
-            return "Scissors beats Paper! You Lose!";
-        }
-        return "Paper beats Rock! You Win!";
-    }
-    if(choice==='scissors'){
-        if(computerChoice==='rock'){
-            return "Rock beats Scissors! You Lose!";
-        }
-        return "Rock beats Scissors! You Win!";
-    }
-    return "invalid input";
 }
-function game(){
-    let userInput;
-    let cpuInput;
-    let wins = 0, losses = 0, ties = 0
-    for(let i=0; i<5; i++){
-        userInput = prompt("Choose rock, paper, or scissors");
-        cpuInput = getComputerChoice();
-        str = playRound(userInput, cpuInput)
-        if(str.slice(-2) === 'g.'){
-            ties+=1
-        }
-        else if(str.slice(-2)==='n!'){
-            wins+=1
-        }
-        else if(str.slice(-2)==='e!'){
-            losses+=1
+function playRound(playerChoice){
+    const computerChoice = getComputerChoice();
+    let msg = '';
+    if (playerChoice === computerChoice){
+        msg ="Tie! You both chose the same thing.";
+        ties += 1;
+    }
+    else if(playerChoice==='rock'){
+        if(computerChoice==='paper'){
+            msg = "Paper beats Rock! You Lose!";
+            losses += 1;
         }
         else{
-            i-=1;
-            console.log('choose again')
+            msg = "Rock beats Scissors! You Win!";
+            wins+=1;
         }
-        console.log(str);
-        console.log(`Wins: ${wins}, Losses: ${losses}, Ties: ${ties}`);
     }
-    if(wins>losses){
-        console.log('Congradumalations! You won the series!');
+    else if(playerChoice==='paper'){
+        if(computerChoice==='scissors'){
+            msg = "Scissors beats Paper! You Lose!";
+            losses += 1;
+        }
+        else{
+            msg = "Paper beats Rock! You Win!";
+            wins+=1;
+        }
     }
-    else if(losses>wins){
-        console.log('You lost! Get better bruh!');
+    else {
+        if(computerChoice==='rock'){
+            msg = "Rock beats Scissors! You Lose!";
+            losses+=1;
+        }
+        else{
+            msg = "Rock beats Scissors! You Win!";
+            wins+=1;
+        }
     }
-    else{
-        console.log('It is a tie!');
-    }
+    gameResult.textContent = msg;
+    displayScore();
 }
-game();
+rockButton.addEventListener('click', () => console.log(playRound('rock')));
+paperButton.addEventListener('click', () => console.log(playRound('paper')));
+scissorsButton.addEventListener('click', () => console.log(playRound('scissors')));
